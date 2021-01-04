0c2f01a51fb3c4afb4d8b1a3496df62667f6446c
jew-de <69693275+jew-de@users.noreply.github.com>
2021-01-04 19:37:23 +0100

    Comments on App.js
    explained functions in App.js with comments

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from "react";
 import "App.css";
 
+//import components
 import Form from './components/Form.js';
 import TodoList from './components/TodoList.js';
 
 function App() {
-
+  // called when either the filter or the todos change
+  // set the filtered todos list using the filter value
   const filterTodos = () => {
     switch(filter) {
       case "completed":
@@ -24,12 +26,13 @@ function App() {
   const [todos, setTodos] = useState([]);
   const [filter, setFilter] = useState("all");
   const [filteredTodos, setFilteredTodos] = useState([]);
-
+  // called when todos or filter value change
+  // call filterTodos()
   useEffect(() => {
     console.log("hey");
     filterTodos();
   }, [todos, filter]);
-
+  
   return (
     <div>
       <Form 
