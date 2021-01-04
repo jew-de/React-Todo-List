79c9bd12742e51f61aba6054acb61c8d01220e84
jew-de <69693275+jew-de@users.noreply.github.com>
2021-01-04 19:38:44 +0100

    Comment Form.js

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,24 +1,34 @@
 import React from 'react';
 
 const Form = ({ setInputText, inputText, todos, setTodos, setFilter }) => {
+    // called when the value of the input field changes
+    // change the inputText state to the value of the input
     const inputTextHandler = (e) => {
         setInputText(e.target.value);
     };
+    // called when the create button is pressed
+    // create a new todo:
+    // text: value of input
+    // completed: false, as it is not completetd by default
+    // id: a random number
+    // set the inputText state to an empty string
     const addHandler = (e) => {
         e.preventDefault();
         setTodos([
-            ...todos, {text: inputText, completed: false, id: Math.random() * 1000}
+            ...todos, {text: inputText, completed: false, id: Math.random() * 1000, priority: "1"}
         ]);
         setInputText("");
     };
+    // called when the value of the select changes
+    // set filter state to the value of the select
     const filterHandler = (e) => {
         setFilter(e.target.value);
     };
     return ( 
         <form>
-            <input type="text"  onChange={inputTextHandler} value={inputText}></input>
-            <button type="submit" onClick={addHandler}>Create</button>
-            <select onChange={filterHandler}>
+            <input type="text"  onChange={inputTextHandler} value={inputText} className="form-input"></input>
+            <button type="submit" onClick={addHandler} className="form-button">Create</button>
+            <select onChange={filterHandler} className="form-select">
                 <option value="all">All</option>
                 <option value="completed">Completed</option>
                 <option value="uncompleted">Uncompleted</option>
