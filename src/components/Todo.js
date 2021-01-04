0b46ebb79e4e0600d1a1f5a371db4dd72eb66ede
jew-de <69693275+jew-de@users.noreply.github.com>
2021-01-04 19:39:35 +0100

    Comment Todo.js

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
 const Todo = ({ todo, todos, setTodos }) => {
+    // called when delete button is pressed
+    // delete the todo of which the delete button got pressed
     const deleteHandler = () => {
         setTodos(todos.filter((item) => item.id !== todo.id));
     };
+    // called when complete button is pressed
+    // invert the completed property of the todo of which the delete button got pressed 
     const completeHandler = () => {
         setTodos(todos.map((item) => {
             if(item.id === todo.id) {
@@ -14,13 +18,14 @@ const Todo = ({ todo, todos, setTodos }) => {
             return item;
         }));
     };
+    var classes = todo.completed ? "completed" + " todo" : "" + "todo";
     return (
         <div>
-            <li className={`${todo.completed ? "completed": ""}`}>
-                {todo.text}
+            <li className={classes}>
+                {todo.text + " "}
+                <button onClick={completeHandler} className="todo-complete-button">{`${todo.completed ? "Uncomplete": "Complete"}`}</button>
+                <button onClick={deleteHandler} className="todo-delete-button">Delete</button>
             </li>
-            <button onClick={completeHandler}>{`${todo.completed ? "Uncomplete": "Complete"}`}</button>
-            <button onClick={deleteHandler}>Delete</button>
         </div>
     );
 };
